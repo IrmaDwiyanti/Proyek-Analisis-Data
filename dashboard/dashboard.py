11cd28d37219fe70b3d9b2202aa6b65440730bba
Irma Dwiyanti <irmadwiyanti.sky@gmail.com>
2025-03-09 01:41:33 +0700

    update dashboard

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 # Load data dari CSV
-data = pd.read_csv('main_data.csv')
+data = pd.read_csv(r'D:\0LATIHAN\DBS\Analisis data dengan Python\submission\dashboard\main_data.csv')
 
 # Judul Dashboard
 st.title('Dashboard Penjualan dan Pelanggan')
